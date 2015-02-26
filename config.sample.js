81d436a3dd1eaa8166f0d47f9bc803cc6c2ffc59
Thomas Hart <tom@thomashart.me>
2015-02-26 03:38:10 +0000

    Basic functionality achieved (in debug mode)

---
+++ config.sample.js
@@ -0,0 +1,6 @@
+var config = {}
+
+config.slackbot = {};
+config.slackbot.host_name = "[YOUR_SLACK_TEAM].slack.com"
+config.slackbot.path = "/services/hooks/slackbot?token=[YOUR_SLACKBOT_TOKEN]"
+module.exports = config;
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var http = require('http');
+var https = require('https');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+var config = require('./config');
+
 var user_hash = {};
 
-// body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // test route
 app.get('/', function (req, res) {
-  var response = "Hello, pomo slack.";
+  var response = "Ping? Pong!";
   res.status(200).send(response);
 });
 
+// Slack slash command route
 app.post('/slack', function (req, res) {
   console.log(req.body);
   
@@ -41,16 +45,74 @@ app.post('/slack', function (req, res) {
 });
  
 function pomo_start(user_name) {
-  user_hash[user_name] = Date.now();
+  start_pomo_timer(user_name);
   return "Pomodoro timer has been started. 25 minutes remain.";
 }
 
+function start_pomo_timer(user_name) {
+  if(user_hash[user_name] == undefined) {
+    user_hash[user_name] = {};
+  }
+  
+  var u = user_hash[user_name];
+  
+  u.time_start = new Date();
+  
+  if(u.type == "work") {
+    u.type = "break";
+    u.timer = 5 * 60 * 1000;
+    
+    // TODO DEBUG
+    u.timer = 10;
+  }
+  else {
+    u.type = "work";
+    u.timer = 25 * 60 * 1000;
+    
+    // TODO DEBUG
+    u.timer = 10;
+  }
+  
+  setTimeout(pomo_done(user_name), u.timer);
+}
+
 function pomo_status(user_name) {
-  return new String(user_hash[user_name]);
+  return JSON.stringify(user_hash[user_name]);
 }
 
 function pomo_info() {
-  return "Pomodoro timer https://github.com/myrridin/slack-pomo-iojs"
+  return "Pomodoro timer https://github.com/myrridin/slack-pomo-iojs";
+}
+
+function pomo_done(user_name) {
+  var channel = "%40" + user_name;
+  var api_path = config.slackbot.path + "&channel=" + channel;
+  
+  var post_data = "Pomo timer complete! That ends the *" + 
+    user_hash[user_name].type + 
+    "* phase. Type `/pomo start` to start the next phase.";
+    
+  var post_options = {
+      host: config.slackbot.host_name,
+      port: '443',
+      path: api_path,
+      method: 'POST',
+      headers: {
+          //'Content-Type': 'raw',
+          'Content-Length': Buffer.byteLength(post_data)
+      }
+  };
+
+  var post_req = https.request(post_options, function(res) { });
+  console.log(post_options);
+  
+  post_req.on('error', function(e) {
+    console.log(e);
+  });
+  
+  post_req.write(post_data);
+  post_req.end();
+  
 }
 
 // error handler
